7f8bba2870b0239adf254272356a2270317452a0
Joyce Pereira Oliveira <98670029+erickbarauna@users.noreply.github.com>
2023-05-04 19:35:10 -0300

    estilizando

--- exercicios/Aula 14 (180423) projetoalpha/projetoalpha_v.2/produto.php
+++ exercicios/Aula 14 (180423) projetoalpha/projetoalpha_v.2/produto.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tela Produto</title>
-    <link rel="stylesheet" href="cleito.css">
+    <link rel="stylesheet" href="pica.css">
 </head>
 <body>
     <header>
@@ -13,25 +13,43 @@
     </header>
     <main>
         <div class="produto">
-            <img src="img/dodge-viper-500.jpg" alt="">
-            <div class="preco">
+            <img src="img/dodge-viper-700.jpg" alt="">
+            <div class="desc">
                 <div class="titulo">
-                    <h1>DODGE</h1><h1 class="destaque">VIPER</h1>
+                    <h1>DODGE</h1><strong>VIPER</strong>
                 </div>
-                <h2>R$ 1.200.000,00</h2>
-                <button>COMPRAR</button>
+                <p>8.4 SRT V10 GASOLINA 2P MANUAL</p>
+
+                <ul class="sem-estilo">
+                    <li>
+                        <p>carro</p>
+                        <p>carro</p>
+                    </li>
+                    <li>
+                        <p>carro</p>
+                        <p>carro</p>
+                    </li>
+                    <li>
+                        <p>carro</p>
+                        <p>carro</p>
+                    </li>
+                    <li>
+                        <p>carro</p>
+                        <p>carro</p>
+                    </li>
+                    <li>
+                        <p>carro</p>
+                        <p>carro</p>
+                    </li>
+                </ul>
+                
+                
+                <div class="valor">
+                    <h2>R$ 1.200.000,00</h2>
+                    <button>COMPRAR</button>
+                </div>
             </div>
         </div>
-        <!-- <div class="descricao">
-            <h2>Descrição do Produto</h2>
-            <p>Preparado pela Hennessey com as seguintes alterações: filtros de ar K&N / long tubes e catback, finalizado pelo Galpão Z/28 com uma pintura especial de Verde / adesivos no capô e no teto / aerofólio em fibra de carbono APR Performance / spoiler dianteiro em fibra de carbono e shift light digital da MSD Powersports, câmbio manual, multimídia Pionner com Android Auto, Apple CarPlay e TV Digital, rodas HRE Performance forjadas aro 19'/20' pinças de freio em vermelho, em exposição no nosso showroom. Outros Opcionais: Porta-copos, Kit Multimídia, MP3 Player, Pára-choques na cor do veículo.</p>
-        </div> -->
     </main>
-    <section>
-    <div class="descricao">
-            <h2>Descrição do Produto</h2>
-            <p>Preparado pela Hennessey com as seguintes alterações: filtros de ar K&N / long tubes e catback, finalizado pelo Galpão Z/28 com uma pintura especial de Verde / adesivos no capô e no teto / aerofólio em fibra de carbono APR Performance / spoiler dianteiro em fibra de carbono e shift light digital da MSD Powersports, câmbio manual, multimídia Pionner com Android Auto, Apple CarPlay e TV Digital, rodas HRE Performance forjadas aro 19'/20' pinças de freio em vermelho, em exposição no nosso showroom. Outros Opcionais: Porta-copos, Kit Multimídia, MP3 Player, Pára-choques na cor do veículo.</p>
-        </div>
-    </section>
 </body>
 </html>
