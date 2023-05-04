9726e77ddde80d9ebf1dce3f80c903cc5ef80755
Joyce Pereira Oliveira <98670029+erickbarauna@users.noreply.github.com>
2023-05-04 08:35:37 -0300

    estilizando a tela produto

--- exercicios/Aula 14 (180423) projetoalpha/projetoalpha_v.2/produto.php
+++ exercicios/Aula 14 (180423) projetoalpha/projetoalpha_v.2/produto.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tela Produto</title>
-    <link rel="stylesheet" href="estilo.css">
+    <link rel="stylesheet" href="cleito.css">
 </head>
 <body>
     <header>
@@ -15,14 +15,23 @@
         <div class="produto">
             <img src="img/dodge-viper-500.jpg" alt="">
             <div class="preco">
-                <h1>DODGE VIPER</h1>
+                <div class="titulo">
+                    <h1>DODGE</h1><h1 class="destaque">VIPER</h1>
+                </div>
                 <h2>R$ 1.200.000,00</h2>
                 <button>COMPRAR</button>
             </div>
         </div>
-        <!-- <div>
+        <!-- <div class="descricao">
+            <h2>Descrição do Produto</h2>
             <p>Preparado pela Hennessey com as seguintes alterações: filtros de ar K&N / long tubes e catback, finalizado pelo Galpão Z/28 com uma pintura especial de Verde / adesivos no capô e no teto / aerofólio em fibra de carbono APR Performance / spoiler dianteiro em fibra de carbono e shift light digital da MSD Powersports, câmbio manual, multimídia Pionner com Android Auto, Apple CarPlay e TV Digital, rodas HRE Performance forjadas aro 19'/20' pinças de freio em vermelho, em exposição no nosso showroom. Outros Opcionais: Porta-copos, Kit Multimídia, MP3 Player, Pára-choques na cor do veículo.</p>
         </div> -->
     </main>
+    <section>
+    <div class="descricao">
+            <h2>Descrição do Produto</h2>
+            <p>Preparado pela Hennessey com as seguintes alterações: filtros de ar K&N / long tubes e catback, finalizado pelo Galpão Z/28 com uma pintura especial de Verde / adesivos no capô e no teto / aerofólio em fibra de carbono APR Performance / spoiler dianteiro em fibra de carbono e shift light digital da MSD Powersports, câmbio manual, multimídia Pionner com Android Auto, Apple CarPlay e TV Digital, rodas HRE Performance forjadas aro 19'/20' pinças de freio em vermelho, em exposição no nosso showroom. Outros Opcionais: Porta-copos, Kit Multimídia, MP3 Player, Pára-choques na cor do veículo.</p>
+        </div>
+    </section>
 </body>
 </html>
